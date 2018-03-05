1746becf8c162991b4fa17627e7adc2d4603a341
Robbie McArthur <2098323m@student.gla.ac.uk>
2018-03-05 23:43:59 +0000

    basic html, views, url mapping

--- sportsday/views.py
+++ sportsday/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_page(request):
+    context_dict = {'boldmessage': "Welcome to SportsDay!"}
+    return render(request, 'sportsday/home_page.html', context=context_dict)
+
+def activities(request):
+    return render(request, 'sportsday/activities.html')
+
+def matches(request):
+    return render(request, 'sportsday/matches.html')
+
+def register(request):
+    return render(request, 'sportsday/register.html')
+
+def messages(request):
+    return render(request, 'sportsday/messages.html')
+
+def contact(request):
+    return render(request, 'sportsday/contact.html')
--- sportsday_project/urls.py
+++ sportsday_project/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from sportsday import views
 
 urlpatterns = [
+    url(r'^$', views.home_page, name='home_page'),
+    url(r'^activities/$', views.activities, name='activities'),
+    url(r'^matches/$', views.matches, name='matches'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^messages/$', views.messages, name='messages'),
+    url(r'^contact/$', views.activities, name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
