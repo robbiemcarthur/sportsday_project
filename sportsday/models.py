de2bf5e363c6bc2e313af75b6296bd7904f721b3
robbiemcarthur <32882675+robbiemcarthur@users.noreply.github.com>
2018-03-15 13:17:02 +0000

    population script update

--- sportsday/models.py
+++ sportsday/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -25,6 +26,12 @@ class Matches(models.Model):
 
     def __str__(self):
     return self.name
+
+class Category(models.Model)
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Page(models.Model):
     title = models.CharField(max_length=128)
