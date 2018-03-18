b31c7d1ed119eab1c037bd7465a8761a1d97fbf2
Robbie McArthur <2098323m@student.gla.ac.uk>
2018-03-18 00:05:13 +0000

    models, populate update

--- sportsday/admin.py
+++ sportsday/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from sportsday.models import Category, Page, UserProfile, Activity, Match, Message
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Page)
+admin.site.register(UserProfile)
+admin.site.register(Activity)
+admin.site.register(Match)
+admin.site.register(Message)
--- sportsday/models.py
+++ sportsday/models.py
@@ -1,30 +1,51 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 class Activity(models.Model):
     name = models.CharField(max_length=128, unique=True)
     location = models.CharField(max_length=128)
     type = models.CharField(max_length=128)
-    numPlayers = models.IntegerField(default=0, max=5)
+    numPlayers = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     availability = models.DateTimeField(auto_now=False, auto_now_add=False)
     views = models.IntegerField(default=0)
-    id = models.IntegerField(default=0, unique=True)
+    id_num = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Activities'
 
     def __str__(self):
-    return self.name
+        return self.name
 
-class Matches(models.Model):
+
+class Match(models.Model):
     name = models.CharField(max_length=128, unique=True)
     location = models.CharField(max_length=128)
-    players = models.IntegerField(default=0, max=5)
+    players = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     activity_type = models.CharField(max_length=128)
     availability = models.DateTimeField(auto_now=False, auto_now_add=False)
     views = models.IntegerField(default=0)
     matches_played = models.IntegerField(default=0)
-    id = models.IntegerField(default=0, unique=True)
+    id_num = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Matches'
 
     def __str__(self):
-    return self.name
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
 
 class Page(models.Model):
     title = models.CharField(max_length=128)
@@ -34,11 +55,13 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
+
 class Message(models.Model):
     message = models.CharField(max_length=256)
 
     def __str__(self):
         return self.message
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
@@ -46,4 +69,4 @@ class UserProfile(models.Model):
     picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
-            return self.user.username
+        return self.user.username
