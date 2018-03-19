c4ec94ca30e467270e398b54ab133b06a2b862ab
Robbie McArthur <2098323m@student.gla.ac.uk>
2018-03-19 13:49:12 +0000

    can't get population script to run

--- sportsday/models.py
+++ sportsday/models.py
@@ -2,33 +2,21 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-
-# Create your models here.
 class Activity(models.Model):
+    number = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=128, unique=True)
-    location = models.CharField(max_length=128)
-    numPlayers = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
-    views = models.IntegerField(default=1)
-    id_num = models.IntegerField(default=1)
-
-    class Meta:
-        verbose_name_plural = 'Activities'
+    location = models.CharField(max_length=128, null=False)
+    num_players = models.IntegerField(null=False, validators=[MinValueValidator(0), MaxValueValidator(5)])
 
     def __str__(self):
-        return self.name
-
-
+        return self.number
 class Match(models.Model):
-    activity_type = models.CharField(max_length=128)
-    matches_played = models.IntegerField(default=0)
-    matchnum = models.IntegerField(default=1)
-
-    class Meta:
-        verbose_name_plural = 'Matches'
+    match_id = models.IntegerField(primary_key=True)
+    comment = models.CharField(max_length=128)
+    activity = models.ForeignKey(Activity)
 
     def __str__(self):
-        return self.activity_type
-
+        return self.comment
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
